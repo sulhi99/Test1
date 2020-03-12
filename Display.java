1672129e284373b349eb0e97f984a2135c9c56de
Sulhi Aqilah <61222776+sulhi99@users.noreply.github.com>
2020-03-12 15:17:47 +0800

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,11 @@
+public class Display
+{
+  public static void main(String [] args)
+  {
+  int age = 20 ;
+  
+  System.out.println ("My age is");
+  System.out.println(age);
+  
+  }
+}
---
+++ EqualCheck.java
@@ -0,0 +1,18 @@
+public class EqualCheck 
+{
+    public static void main(String [] args)
+    {
+        String a = "SULHI";
+        String b = "sulhi";
+
+        if(a.equals(b))
+        {
+            System.out.println("Both strings are equal.");
+        } 
+        else 
+        {
+            System.out.println("Both strings are not equal.");
+        }
+
+    }
+} 
